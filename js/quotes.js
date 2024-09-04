63075454c1236cb94b6ba859e9880665fd940196
calix00 <dhk08300@gmail.com>
2024-09-04 16:46:45 +0900

    commit

---
+++ js/quotes.js
@@ -0,0 +1,46 @@
+const quotes = [
+    {
+        quote: "Life si full of ups and downs.",
+        author: "",
+    },
+    {
+        quote: "Live as if you were to die tomorrow.",
+        author: "",
+    },
+    {
+        quote: "Believe in your self.",
+        author: "",
+    },
+    {
+        quote: "past is just pat.",
+        author: "",
+    },
+    {
+        quote: "Life is not all beer and skittles",
+        author: "",
+    },
+    {
+        quote: "Until death, it is all life.",
+        author: "",
+    },
+    {
+        quote: "Don't dwell on the past.",
+        author: "",
+    },
+    {
+        quote: "The only cure for grief is action.",
+        author: "",
+    },
+    {
+        quote: "No sweat, No sweet.",
+        author: "",
+    },
+    {
+        quote: "Where there is a will there is a way.",
+        author: "",
+    },
+    {
+        quote: "Waste no Fresh tears over old griefs",
+        author: "",
+    },
+]
